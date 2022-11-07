7ad7c3f86317d684647fa44323060a352ba74a44
Melis Yazıcı <melis.yazici95@icloud.com>
2022-11-07 19:37:12 +0100

    Modify didMove method
    to place and position the background image

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -10,7 +10,11 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background") // load the file called background.jpg
+        background.position = CGPoint(x: 512, y: 384) // to place the background image in the center of a landscape iPad
+        background.blendMode = .replace // draw it, ignoring any alpha values -> which makes it fast for things without gaps such as the background
+        background.zPosition = -1 // draw this behind everything else
+        addChild(background) // to add any node to the current screen
     }
     
 }
